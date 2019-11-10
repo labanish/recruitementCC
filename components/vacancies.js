f0dcb45c2e43ccebc48b72ca70b71265a0ac26e0
lndwaru <lndwaru@LNDWARU-RO.ke.oracle.com>
2019-11-11 00:26:12 +0200

    Changes to make it work

--- components/vacancies.js
+++ components/vacancies.js
@@ -1,9 +1,13 @@
 'use strict';
-//function to determine jobs in a given category
+
 var request = require('request');
-function query(job, callback)
+
+//function to determine jobs in a given category and country
+
+function query(jobt,jobl, callback)
+
 {
-  request({ "uri": "https://nn3dsuzdmbbb7ms-standardatp.adb.uk-london-1.oraclecloudapps.com/ords/labanish/recruitement/vacanciesInfo/"+job,
+  request({ "uri": "https://nn3dsuzdmbbb7ms-standardatp.adb.uk-london-1.oraclecloudapps.com/ords/labanish/recruitement/vacancies/"+jobt+"/"+jobl,
       "method": "GET"
     }, function (err, res1, body) {
         body = JSON.parse(body);
@@ -19,16 +23,20 @@ module.exports = {
     name: 'vacancies',
     properties: {
       jobType: { required: true, type: 'string' },
+      jobLoc : { required: true, type: 'string' }
     },
     supportedActions: []
   }),
   invoke: (conversation, done) => {
     
     const { jobType } = conversation.properties();
+    const { jobLoc } = conversation.properties();
     // determine jobs
+    var jobLocUpper = jobLoc.toUpperCase();
 
+console.log(jobType+ " "+jobLocUpper);
 
-    query(jobType, function (res1, err){
+ query(jobType,jobLocUpper, function (res1, err){
 
   var lst = "";
   if (res1 && res1.items)
@@ -44,16 +52,23 @@ module.exports = {
         lst += res1.items[i].vacancyid +": "+res1.items[i].title;
        
       }
- console.log(lst);
+ //console.log(lst);
 
     conversation.variable("jobTypes", lst);
     conversation.transition();
     done();
     }
+    else
+    {
+      console.log("No items on the selected category");
+      conversation.reply("Unfortunately we do not have any vacancies related to " +jobType.toLowerCase() + " in "+jobLoc);
+      conversation.transition();
+      done();
+    }
   }
   else
   {
-    //console.log("Encountered an error... please try again later or contact the demo administrator");
+    console.log("Encountered an error... please try again later or contact the demo administrator");
     conversation.reply("Encountered an error... please try again later or contact the demo administrator");
     conversation.transition();
     done();
